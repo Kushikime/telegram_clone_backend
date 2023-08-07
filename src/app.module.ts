52976ccdb34924f63bf846b52c67eee3c2838fe0
Erik Demchak <demcsakerik@gmail.com>
2023-08-07 18:48:09 +0200

    update: added docker, users and language resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { LanguagesModule } from './languages/languages.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [LanguagesModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
