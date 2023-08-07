550d17c866544e19fc7a63f3e701835b29a51893
Erik Demchak <demcsakerik@gmail.com>
2023-08-07 20:16:15 +0200

    ADD: added bcrypt and created related services. Connected a DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,28 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LanguagesModule } from './languages/languages.module';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { IamModule } from './iam/iam.module';
+import { HashingService } from './aim/hashing/hashing.service';
+import { BcryptService } from './aim/hashing/bcrypt.service';
 
 @Module({
-  imports: [LanguagesModule, UsersModule],
+  imports: [
+    LanguagesModule,
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'pass123',
+      database: 'postgres',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    IamModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, HashingService, BcryptService],
 })
 export class AppModule {}
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,7 +1,18 @@
-import { ObjectType, Field, Int } from '@nestjs/graphql';
+import { ObjectType, Field, ID } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
+@Entity()
 @ObjectType()
 export class User {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => ID, { description: 'User ID' })
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Field({ description: 'User email' })
+  @Column({ unique: true })
+  email: string;
+
+  @Field({ description: 'User password' })
+  @Column()
+  password: string;
 }
